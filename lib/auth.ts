4020c285150dd9cac36de004a100d781bafb4019
Aayush Gupta <aayushgupta69official@gmail.com>
2025-06-12 02:15:56 +0530

    Fixed an issue in BetterAuth initialization.

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,13 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
 import { db } from "@/database";
+import * as schema from "@/database/schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema: { ...schema },
   }),
   emailAndPassword: {
     enabled: true,
